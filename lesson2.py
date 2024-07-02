b71ad3219ff9181e1deb4eb69e8f9e0afb0fbb5e
ViktorGolikoff <pravgolvik@gmail.com>
2024-07-02 18:17:56 +0900

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+course_name = "Python"
+tasks_count = 12
+spent_time = 1.5
+time_do = spent_time/tasks_count
+
+print ("Курс:", course_name, ", всего задач:", tasks_count, ", затрачено часов:", spent_time, ", среднее время выполнения", time_do, ".")
+
+
